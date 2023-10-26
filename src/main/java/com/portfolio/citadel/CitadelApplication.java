da2b847233216c8fd826552a815f57a46ec3b1a7
suhpark <suhpark@gmarket.com>
2023-10-26 12:48:13 +0900

    start.spring.io

---
+++ src/main/java/com/portfolio/citadel/CitadelApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.citadel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitadelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitadelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/citadel/CitadelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.citadel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitadelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
