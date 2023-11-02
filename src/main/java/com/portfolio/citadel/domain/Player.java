b6600d775435d1a25b5e929a5e32036f6d82f0fd
suhpark <suhpark@gmarket.com>
2023-11-02 22:54:20 +0900

    빌딩 건설

--- src/main/java/com/portfolio/citadel/domain/Player.java
+++ src/main/java/com/portfolio/citadel/domain/Player.java
@@ -2,11 +2,17 @@ package com.portfolio.citadel.domain;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 public class Player {
     private int no;
     private Job job;
     private boolean crown;
+    private int money;
+    private List<Building> hands = new ArrayList<>();
+    private List<Building> buildings = new ArrayList<>();
 
     public Player(int no, boolean isCrown) {
         this.no = no;
@@ -19,5 +25,13 @@ public class Player {
 
     public void setCrown(boolean crown) {
         this.crown = crown;
+    }
+
+    public void setMoney(int money) {
+        this.money += money;
+    }
+
+    public void addHand(Building building) {
+        this.getHands().add(building);
     }
 }
--- src/main/java/com/portfolio/citadel/service/GameService.java
+++ src/main/java/com/portfolio/citadel/service/GameService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,27 +29,114 @@ public class GameService {
     private final static int memberCount  = 6;
     private final static int round = 8;
 
+    private static List<Job> jobList = new ArrayList<>();
+    private static Queue<Building> buildingDeck = new LinkedList<>();
+
     public void play() {
 
-        PlayerWrapper pw = new PlayerWrapper();
-        List<Job> jobList = jobRepository.findAll().stream().map(Job::new).collect(Collectors.toList());
-
-        List<Building> buildingDeck = new ArrayList<>();
+        // List<Job> jobList = jobRepository.findAll().stream().map(Job::new).collect(Collectors.toList());
+        jobList = jobRepository.findAll().stream().map(Job::new).collect(Collectors.toList());
+        List<Building> buildingList = new ArrayList<>();
         buildingRepository.findAll().stream()
                 .forEach(b -> {
                     for(int i=0; i<b.getCount(); i++) {
-                        buildingDeck.add(new Building(b));
+                        buildingList.add(new Building(b));
                     }
                 });
-        Collections.shuffle(buildingDeck);
+        Collections.shuffle(buildingList);
+        buildingDeck = new LinkedList<>(buildingList);
+        PlayerWrapper pw = new PlayerWrapper(memberCount);
 
-        for(int i=1; i<=round; i++) {
-            log.info(String.format("[Round %d]", i));
+        this.init(pw.getPlayerList(), buildingDeck);
+
+        boolean isFinalRound = false;
+        int round = 1;
+        while(!isFinalRound) {
+            log.info(String.format("[Round %d]", round));
 
             pw.chooseJob(jobList);
-            pw.doAction(jobList);
+            this.doAction(pw.getPlayerList());
 
-            log.info(String.format("[Round %d end]", i));
+            log.info(String.format("[Round %d end]", round));
+            round++;
+
+            Optional<Player> SevenBuildingOwner = pw.getPlayerList().stream()
+                    .filter(p -> p.getBuildings().size() == 7)
+                    .findAny();
+            if(SevenBuildingOwner.isPresent()) isFinalRound = true;
         }
+    }
+
+    private void init(List<Player> playerList, Queue<Building> buildingDeck) {
+
+        for(Player p : playerList) {
+            p.setMoney(2);
+            for(int i=0; i<4; i++) {
+                p.addHand(buildingDeck.poll());
+            }
+        }
+    }
+
+    private void doAction(List<Player> playerList) {
+
+        for(Job job : jobList) {
+            Optional<Player> player = playerList.stream().filter(p -> p.getJob().getNo() == job.getNo()).findAny();
+            if(player.isPresent()) {
+                log.info(String.format("%s의 차례입니다. Player%d가 행동합니다. (%d/%d/%d)", player.get().getJob().getName(), player.get().getNo(), player.get().getMoney(), player.get().getHands().size(), player.get().getBuildings().size()));
+                if(player.get().getJob().isKing()) {
+                    log.info(String.format("왕관을 가져옵니다."));
+                    playerList.stream().filter(p_-> p_.isCrown()).findAny().get().setCrown(false);
+                    player.get().setCrown(true);
+                }
+
+                this.getAsset(player.get());
+                this.build(player.get());
+
+                log.info(String.format("턴 종료. (%d/%d/%d)", player.get().getMoney(), player.get().getHands().size(), player.get().getBuildings().size()));
+            }
+        }
+    }
+
+    private void getAsset(Player player) {
+        if(this.yesOrNo()) {
+            // 은행으로부터 금화 2개 받기
+            player.setMoney(2);
+            log.info(String.format("Player%d가 금화 2개를 받았습니다. (총 %d개)", player.getNo(), player.getMoney()));
+        }
+        else {
+            // 건물 카드 가져오기
+            player.getHands().add(buildingDeck.poll());
+            player.getHands().add(buildingDeck.poll());
+
+            Random random = new Random();
+            int index = random.nextInt(player.getHands().size());
+            Building building = player.getHands().get(index);
+            player.getHands().remove(building);
+            buildingDeck.offer(building);
+            log.info(String.format("Player%d가 건물 카드를 받았습니다.", player.getNo()));
+        }
+    }
+
+    private void build(Player player) {
+
+        // 건물 짓기
+        List<Building> buildable = player.getHands().stream()
+                .filter(b -> b.getCost() <= player.getMoney())
+                .collect(Collectors.toList());
+
+        if(buildable.size() > 0) {
+            Random random = new Random();
+            int index = random.nextInt(buildable.size());
+
+            Building building = buildable.get(index);
+            player.setMoney(-1 * building.getCost());
+            player.getHands().remove(building);
+            player.getBuildings().add(building);
+            log.info(String.format("Player%d가 [%s(%d)]을 지었습니다. (총 %d개)", player.getNo(), building.getName(), building.getCost(), player.getBuildings().size()));
+        }
+    }
+
+    private boolean yesOrNo() {
+        return (int)(Math.random() * 2) == 1;
     }
 }
